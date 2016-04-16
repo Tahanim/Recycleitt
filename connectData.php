73c7d044b8fa22ae2dbe75938d551c5ccff76d67
Tahanim <ttahanii.m@gmail.com>
2016-04-16 14:30:23 +0300

    Update connectData.php

--- connectData.php
+++ connectData.php
@@ -1,5 +1,5 @@
 <?php
-$connect = mysql_connect('br-cdbr-azure-south-b.cloudapp.net','bb13edd72d8934','c8330e63') or die ("Can not connect to server");
+$connect = mysql_connect('br-cdbr-azure-south-b.cloudapp.net','bb13edd72d8934','c8330e63','recycle_items') or die ("Can not connect to server");
 $data = mysql_select_db('recycle_items',$connect) or die ("Can not connect to Recycle database");
 
 
