fe8389c61103563a83803849d501b88c16fa8f84
Tahanim <ttahanii.m@gmail.com>
2016-05-19 14:35:49 +0300

    Update connectData.php

--- connectData.php
+++ connectData.php
@@ -3,9 +3,4 @@ $connect = mysql_connect('br-cdbr-azure-south-b.cloudapp.net','bb13edd72d8934','
 $data = mysql_select_db('recycle_items',$connect) or die ("Can not connect to Recycle database");
 
 
-// DB connection info
-$host = "br-cdbr-azure-south-b.cloudapp.net";
-$user = "bb13edd72d8934";
-$pwd = "c8330e63";
-$db = "recycle_items";
 ?>
